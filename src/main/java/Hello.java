d82d0d4c45d8a4528f6cf3cb5196e9395d882784
Dzmitry Kislo <kislodim@gmail.com>
2013-07-17 16:37:53 +0300

    added src

---
+++ src/main/java/Hello.java
@@ -0,0 +1,13 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: dzmitry
+ * Date: 7/17/13
+ * Time: 4:36 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("Hello!!!");
+    }
+}
